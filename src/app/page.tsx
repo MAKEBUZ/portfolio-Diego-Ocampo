77a5cef0d6ddc869197cef4c126b969dd954b1f5
Diego Alejandro Ocampo Madroñero <134111264+MAKEBUZ@users.noreply.github.com>
2025-03-12 08:26:37 -0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center w-screen h-screen ">
+    <main className="flex flex-col items-center justify-center w-screen h-screen bg-cyan-300">
       <h1 className="text-cyan-700 text-2xl font-bold">Diego Alejandro Ocampo Madroñero</h1>
     </main>
   );
