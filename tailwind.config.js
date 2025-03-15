95784c2817f847926e5fee04fdfe599593546ae6
Diego Alejandro Ocampo Madroñero <134111264+MAKEBUZ@users.noreply.github.com>
2025-03-15 14:54:32 -0500

    Paleta de Colores

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  darkMode: false, 
+  theme: {
+    extend: {
+        primary: '--primary',
+        secondary: '--secondary',
+        tertiary: '--tertiary',
+        quaternary: '--quaternary',
+        light: '--light',
+        lighter: '--lighter',
+        muted: '--muted',
+        soft: '--soft',
+        accent: '--accent',
+        dark: '--dark',
+        darker: '--darker',
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
